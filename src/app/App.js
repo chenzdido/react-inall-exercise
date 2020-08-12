07437c6140ac28381cb8df140d9fd27f2f079045
chenzdido <chenzdido@gmail.com>
2020-08-13 00:05:00 +0800

    [zhe] feat:complete homepage.

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter, Switch, Link} from "react-router-dom";
 import Home from "./Home";
+import Calculator from './Calculator/Calculator';
+import Countdown from './Countdown/Countdowm';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+        <div className="navigation">
+          <ul className="nav">
+            <li>
+            <Link to='/' className="link">Home</Link>
+            </li>
+            <li>
+            <Link to='/products' className="link">在线计算器</Link>
+            </li>
+            <li className="link">
+              <Link to='/my-profile' className="link">在线倒计时器</Link>
+            </li>
+          </ul>
+        </div>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/calculator' component={Calculator}/>
+            <Route exact path='/countdown' component={Countdown}/>
+          </Switch>
         </BrowserRouter>
       </div>
     );
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Route, BrowserRouter} from "react-router-dom";
 import './home.less';
+import Calculator from './Calculator/Calculator';
 
 const Home = () => {
-  return (<div className="home">
-    This is home page
-  </div>);
+  return (
+  <div className="home">
+    <div className="tool">
+      <div className="inner"> </div>
+      <p className="word">在线实用工具</p>
+    </div>
+    <div className='toolist'>
+      <div className='timer'>
+        <img src='timer.png' alt=''/>
+        <a href="http://localhost:1234/countdown">倒计时器</a>
+      </div>
+      <div className='calculator'>
+        <img src='calculator.png' alt=''/>
+        <a href="http://localhost:1234/calculator">计算器</a>
+      </div>
+    </div>
+   </div>);
 };
 
 export default Home;
